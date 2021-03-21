fe3a5538623c06084011770c5cd95a770779b3b5
Gipost <79999468+Gipost@users.noreply.github.com>
2021-03-21 14:42:18 +0300

    using own lists

--- Main.java
+++ Main.java
@@ -1,35 +1,22 @@
 package com.edu;
+import com.edu.spisks.*;
 import java.util.Scanner;
-import java.util.ArrayList;
 
 
 public class Main {
 
-    public static void main(String[] args)
-    {
-        ArrayList<Integer> IntList = new ArrayList<Integer>();
-        System.out.print("Enter N: \n");
-        Scanner in = new Scanner(System.in);
-        int N = in.nextInt();
-        System.out.print("Enter numbers: \n");
-        for (int i = 0; i<N; i++)
-        {
-            IntList.add(0,in.nextInt());
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of elements: ");
+        int N = sc.nextInt();
+        System.out.println("Enter the elements themselves");
+        ListElement first = null;
+        for (int i = 0; i < N; i++) {
+            int value = sc.nextInt();
+            first = ListUtils.insert(first, value);
         }
-        double res = 1;
-        result(res,0,N,IntList);
+        ListUtils.reduce(first,1);
     }
-    static void result(double res, int n, int N, ArrayList<Integer> Intlist)
-    {
-        if (n<N)
-        {
-            int num = Intlist.get(n);
-            if (num % 3 == 0)
-            {
-                res *= num;
-            }
-            result(res,n+1,N,Intlist);
-        }
-        else System.out.println(res);
-    }
+
 }
+
